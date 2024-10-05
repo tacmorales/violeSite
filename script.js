9134460e3799b6cb677963dac0cd8fcb38df6b8f
TacMorales <tacmorales@gmail.com>
2024-10-05 18:00:25 -0300

    Práctica Viole

---
+++ script.js
@@ -0,0 +1,9 @@
+//Esta es una hoja de javascript
+//Para hacer un menu hamburguesa
+
+//const burguer = document.getElementById('burguer');
+//const menu = document.getElementById('menu');
+
+//burguer.addEventListener('click',()=>{
+//    menu.classList.toggle('show');
+//});
